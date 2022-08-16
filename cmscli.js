0ccfd153d1ee0fa69e2befada83221d7a7df8057
gxy <gxy@gxy.me>
2022-08-16 15:07:35 +0800

    parse args

--- cmscli.js
+++ cmscli.js
@@ -4,13 +4,32 @@ var path = require('path');
 var fs   = require('fs');
 var host = require('./lib/host.js');
 var argv = require('minimist')(process.argv.slice(2));
-
+var config_file = process.env.HOME + '/.config/.cmscli.json';
 host.host = argv.host;
 host.port = parseInt(argv.port, 10);
+if (!host.host || !host.port) {
+    try {
+        var config = JSON.parse(fs.readFileSync(config_file).toString());
+        host.host = config.host || null;
+        host.port = config.port || 0;
+        console.log(config, host);
+    } catch (e) {
+        console.error(e);
+    };
+};
 if (!host.host || !host.port) {
     console.error("Missing host, port", host);
     process.exit();
 };
+if (argv.save) {
+    var config = {
+        host : host.host,
+        port : host.port,
+    }
+    fs.writeFileSync(config_file, JSON.stringify(config, null, 4));
+    console.log("config saved to", config_file);
+    process.exit();
+}
 var func_upload  = require('./lib/upload.js');
 var func_ls      = require('./lib/ls.js');
 var func_find    = require('./lib/find.js');
